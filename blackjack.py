740c3a820cde7d869b54a7de18c491b451b925f6
Joshrtaylor01 <47469594+Joshrtaylor01@users.noreply.github.com>
2019-02-19 21:30:40 +1000

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -1,23 +1,36 @@
 import random
-dealerBust = False
-playerBust = False
-playerHold = False
-dealerHold = False
-endGame = False
-King = Queen = Jack = 10
-Ace = 1
 
-vals = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, Ace]
-cards = ['Two of Spades', 'Three of Spades', 'Four of Spades', 'Five of Spades', 'Six of Spades', 'Seven of Spades', 'Eight of Spades', 'Nine of Spades', 'Ten of Spades', 'Jack of Spades', 'Queen of Spades', 'King of Spades', 'Ace of Spades', 'Two of Hearts', 'Three of Hearts', 'Four of Hearts', 'Five of Hearts', 'Six of Hearts', 'Seven of Hearts', 'Eight of Hearts', 'Nine of Hearts', 'Ten of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts', 'Ace of Hearts', 'Two of Clubs', 'Three of Clubs', 'Four of Clubs', 'Five of Clubs', 'Six of Clubs', 'Seven of Clubs', 'Eight of Clubs', 'Nine of Clubs', 'Ten of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs', 'Ace of Clubs', 'Two of Diamonds', 'Three of Diamonds', 'Four of Diamonds', 'Five of Diamonds', 'Six of Diamonds', 'Seven of Diamonds', 'Eight of Diamonds', 'Nine of Diamonds', 'Ten of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds', 'Ace of Diamonds']
-dealerScore = 0
-playerScore = 0
+endGame = False
+
+Ace = int()
+vals = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, Ace, 2, 3, 4, 5, 6, 7, 8,
+        9, 10, 10, 10, 10, Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, Ace]
+
+cards = ['Two of Spades', 'Three of Spades', 'Four of Spades', 'Five of Spades', 'Six of Spades', 'Seven of Spades',
+         'Eight of Spades', 'Nine of Spades', 'Ten of Spades', 'Jack of Spades', 'Queen of Spades', 'King of Spades',
+         'Ace of Spades', 'Two of Hearts', 'Three of Hearts', 'Four of Hearts', 'Five of Hearts', 'Six of Hearts',
+         'Seven of Hearts', 'Eight of Hearts', 'Nine of Hearts', 'Ten of Hearts', 'Jack of Hearts', 'Queen of Hearts',
+         'King of Hearts', 'Ace of Hearts', 'Two of Clubs', 'Three of Clubs', 'Four of Clubs', 'Five of Clubs',
+         'Six of Clubs', 'Seven of Clubs', 'Eight of Clubs', 'Nine of Clubs', 'Ten of Clubs', 'Jack of Clubs',
+         'Queen of Clubs', 'King of Clubs', 'Ace of Clubs', 'Two of Diamonds', 'Three of Diamonds', 'Four of Diamonds',
+         'Five of Diamonds', 'Six of Diamonds', 'Seven of Diamonds', 'Eight of Diamonds', 'Nine of Diamonds',
+         'Ten of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds', 'Ace of Diamonds']
+
+
+class Player:
+    def __init__(self, score, bust, hold):
+        self.score: int = score
+        self.bust: bool = bust
+        self.hold: bool = hold
+
+
+P = Player(0, False, False)
+D = Player(0, False, False)
 
 
 def deal_card():
-    global playerScore
-    global playerHold
-    if not playerHold:
-        deal = str(input("draw? [1/0]: "))
+    if not P.hold:
+        deal = str(input("draw? [y/n]: "))
         if deal == 'y':
             r = random.randint(0, len(vals))
             c = vals[r]
@@ -30,45 +43,45 @@ def deal_card():
                     global endGame
                     print("you big fact cheat, you lose")
                     endGame = True
-            playerScore += c
-            print(playerScore)
+            P.score += c
+            print(P.score)
             cards.pop(r)
             vals.pop(r)
         elif deal == 'n':
-            playerHold = True
+            P.hold = True
 
 
-def dealerDraw():
-    global dealerHold
-    global dealerScore
-    if not dealerHold and not playerBust:
-        if dealerScore <= 17:
+def dealer_draw():
+    if not D.hold and not P.bust:
+        if D.score <= 17:
             r = random.randint(0, len(vals))
             w = vals[r]
-            dealerScore += w
+            D.score += w
             cards.pop(r)
             vals.pop(r)
         else:
-            dealerHold = True
+            D.hold = True
 
 
-def isBust(score):
+def is_bust(score):
     if score > 21:
         return True
 
 
-def checkWin(dealerScore, playerScore):
+def check_win():
     global endGame
-    if isBust(playerScore) and isBust(dealerScore):
-        print("P: ", playerScore, "D: ", dealerScore)
+    if is_bust(P.score) and is_bust(D.score):
+        print("P: ", P.score, "D: ", D.score)
         print("both of you lost, lol")
         endGame = True
-    elif playerScore == 21 or not isBust(playerScore) and isBust(dealerScore) or playerScore > dealerScore and dealerHold:
-        print("P: ", playerScore, "D: ", dealerScore)
+    elif P.score == 21 or not is_bust(P.score) and is_bust(
+            D.score) or P.score > D.score and D.hold:
+        print("P: ", P.score, "D: ", D.score)
         print(name, 'wins! Good Job!')
         endGame = True
-    elif dealerScore == 21 or isBust(playerScore) and not isBust(dealerScore) or playerScore < dealerScore and playerHold:
-        print("P: ", playerScore, "D: ", dealerScore)
+    elif D.score == 21 or is_bust(P.score) and not is_bust(
+            D.score) or P.score < D.score and P.hold:
+        print("P: ", P.score, "D: ", D.score)
         print("Dealers win! Rough Luck :(")
         endGame = True
 
@@ -76,11 +89,9 @@ def checkWin(dealerScore, playerScore):
 print("Lets play Black jack!")
 name = str(input("name?: "))
 
-
 while not endGame:
     deal_card()
-    isBust(playerScore)
-    dealerDraw()
-    isBust(playerScore)
-    isBust(dealerScore)
-    checkWin(dealerScore, playerScore)
+    is_bust(P.score)
+    dealer_draw()
+    is_bust(D.score)
+    check_win()
